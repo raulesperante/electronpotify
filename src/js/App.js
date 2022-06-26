8e90870ddf6f13e315a789ca5d031f3e7eed93a0
Raúl Esperante <ragusmat@hotmail.com>
2022-06-25 21:31:41 -0300

    Slider current time

--- src/js/App.js
+++ src/js/App.js
@@ -7,8 +7,11 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
@@ -17,9 +20,16 @@ import { ASSETS } from "./helpers/constants";
 
 export default function App() {
 
+    const [value, setValue] = React.useState(30);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+
     return <>
         <Grid container className="main-container">
-            <Grid xs={3}>
+            <Grid item xs={3}>
                 <Grid className="logo-container"><img className="logo" src={ASSETS.LOGO} /></Grid>
                 <Grid className="sidebar">
                     <List>
@@ -57,14 +67,33 @@ export default function App() {
 
                 </Grid>
             </Grid>
-            <Grid xs={9}>
+            <Grid item xs={9}>
                 <div className="mediaplayer__wrapper" style={{ backgroundImage: "url(https://i.imgur.com/sCbrzQa.png)" }}>
                     <div className="mediaplayer">
                         <div className="mediaplayer__header">
                             <div className="mediaplayer__headerName">Hidden</div>
                             <div className="mediaplayer__headerAuthor">by Miaow</div>
+
                         </div>
                         <div className="mediaplayer__controls">
+                            <Slider
+                                sx={{marginTop: "-2em"}}
+                                size="small"
+                                defaultValue={70}
+                                aria-label="Small"
+                            />
+                            <Grid>
+                                <Box></Box>
+                                <Box></Box>
+                                <Box></Box>
+
+                            </Grid>
+                            <Stack className="volume" spacing={2} direction="row" alignItems="center">
+                                <VolumeDown />
+                                <Slider aria-label="Volume" value={value} onChange={handleChange} />
+                                <VolumeUp />
+                            </Stack>
+
                         </div>
                     </div>
                 </div>
